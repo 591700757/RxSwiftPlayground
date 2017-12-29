f648448e66b9aca18296c62e1d89e2670066b2e0
qqqqzift <qqqqzift@msn.com>
2017-12-29 15:18:02 +0900

    Initial Commit

---
+++ VideoSync/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoSync
+//
+//  Created by zhang_ying on 2017/12/29.
+//  Copyright © 2017年 zhang_ying. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
