3fe111734befbab0a50a46b203c93b32ce5ea3a2
qqqqzift <qqqqzift@msn.com>
2017-12-29 16:50:42 +0900

    add missing

---
+++ Pods/Target Support Files/Pods-VideoSync/Pods-VideoSync-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VideoSync : NSObject
+@end
+@implementation PodsDummy_Pods_VideoSync
+@end
---
+++ Pods/Target Support Files/Pods-VideoSyncTests/Pods-VideoSyncTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_VideoSyncTests : NSObject
+@end
+@implementation PodsDummy_Pods_VideoSyncTests
+@end
